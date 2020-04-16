0f4598ed5a426eb01d75a374cc35dc34a99134e2
ziads <438101266@student.ksu.edu.sa>
2020-04-16 05:09:01 +0300

    za

--- CheckUser.php
+++ CheckUser.php
@@ -1,10 +1,36 @@
+
 <?php
-// To check User
-// Hello Ziyad
 
-//hhhhhhhhhh
 
-// Commit test
 
-//commit test2
 
+$aa= mysqli_connect('localhost','root','');
+
+mysqli_select_db($aa,'rregestration');
+
+
+$email= $_POST['EMAIL'];
+$password= $_POST['PASSWORD'];
+
+$selected = "select * from usertable where email = '$email' && password ='$password' " ;
+
+$Result = mysqli_query($aa,$selected);
+
+$counter = mysqli_num_rows($Result) ;
+
+if($counter==1){
+
+    setcookie('vaa','yes',time()+(3600*3),'/');
+
+
+    header('location:UserPage.php');
+}
+
+else{
+
+    header('location:login.php');
+}
+
+
+
+?>
